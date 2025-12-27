dae32bdb3ae8480b5b5e12f8931f78c8ce65344c
Oscar Guerrero <oscard.guerrerom@outlook.com>
2025-12-27 15:03:04 -0500

    Firts commit

---
+++ frontend-js/.next/dev/build/postcss.js
@@ -0,0 +1,6 @@
+var R=require("./chunks/[turbopack]_runtime.js")("postcss.js")
+R.c("chunks/[turbopack-node]_transforms_postcss_ts_77e62812._.js")
+R.c("chunks/[root-of-the-server]__781785e8._.js")
+R.m("[turbopack-node]/globals.ts [postcss] (ecmascript)")
+R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/OneDrive/Escritorio/Programming/NextJs-Strapi/frontend-js/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)")
+module.exports=R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/OneDrive/Escritorio/Programming/NextJs-Strapi/frontend-js/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)").exports
